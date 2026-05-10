08e27b8920db047d0f524051daa10d4c230433a1
Raya Mahardika <94042378+rktdnt@users.noreply.github.com>
2026-05-10 18:45:17 +0700

    fixup! surya: overlay: Reduce blur radius

--- device.mk
+++ device.mk
@@ -269,6 +269,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     J20CCarrierConfig \
     J20CFrameworks \
+    J20CLauncher3 \
     J20CSettings \
     J20CSettingsProvider \
     J20CSystemUI \
