halcyonlv/device_xiaomi_surya

surya: Inherit MiuiCamera if it exists
Change-Id: Iade4ce0c08e9493292b3c9d6a8044b6e

device.mk
@@ -17,6 +17,9 @@
 # Inherit from sm6150-common
 $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 
+# MiuiCamera
+$(call inherit-product-if-exists, vendor/xiaomi/surya-miuicamera/miuicamera.mk)
+
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 29
 

plrine/android_device_xiaomi_surya

device.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# MiuiCamera
+$(call inherit-product-if-exists, device/xiaomi/miuicamera-surya/device.mk)
+
 # A/B OTA
 AB_OTA_UPDATER := false
 