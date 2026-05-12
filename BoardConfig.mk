a5296815ff940b878d24b1751cd814ff90fe0da3
Raya Mahardika <94042378+rktdnt@users.noreply.github.com>
2026-05-12 22:23:48 +0700

    surya: Use xiaomi-sm6150 kernel
    Change-Id: I64bdcf4f8e0b4e927bc9c1cd67d2d1403825fdcb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,7 +85,7 @@ BOARD_MKBOOTIMG_ARGS := --header_version 2
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
-TARGET_KERNEL_CONFIG := surya_defconfig
+TARGET_KERNEL_CONFIG := vendor/sdmsteppe-perf_defconfig vendor/surya.config
 TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
 
 # Partitions
